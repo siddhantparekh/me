2a9e0ff7af99bbfec6f2c526bd0f244b448964a9
Siddhant <parekh.siddhant@research.iiit.ac.in>
2021-05-26 18:43:02 +0530

    Updates

---
+++ precache-manifest.5560ee31fad1cab2dde41124cecb3095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "101fcf3984ed4be02deb",
+    "url": "/me/static/css/main.6eaf0c6a.chunk.css"
+  },
+  {
+    "revision": "101fcf3984ed4be02deb",
+    "url": "/me/static/js/main.1899f089.chunk.js"
+  },
+  {
+    "revision": "c0826cd5ced6d297e690",
+    "url": "/me/static/js/runtime~main.a4106ec5.js"
+  },
+  {
+    "revision": "81b4900aa641e2c3cb78",
+    "url": "/me/static/js/2.31ab9273.chunk.js"
+  },
+  {
+    "revision": "d8f3db9c26d097dc8e28",
+    "url": "/me/static/js/3.37201157.chunk.js"
+  },
+  {
+    "revision": "154ab7724ee7d81f9756a955ee2b01ec",
+    "url": "/me/index.html"
+  }
+];
